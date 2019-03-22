b6e38ce8f27c96ea55eb76634d38f81bc6469465
admin <850340806@qq.com>
2019-03-22 14:44:58 +0800

    second

--- app/src/main/java/com/chinamall21/mobile/kotlinstudy/bean/LoginResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.chinamall21.mobile.kotlinstudy.bean
-
-
-/**
- * desc：
- * author：Created by xusong on 2019/2/28 15:14.
- */
-
-data class LoginResponse(val msg: String, val result: LoginResultResponse, val retCode: String)
--- app/src/main/java/com/chinamall21/mobile/kotlinstudy/bean/LoginResultResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.chinamall21.mobile.kotlinstudy.bean
-
-
-/**
- * desc：
- * author：Created by xusong on 2019/2/28 15:15.
- */
-
-class LoginResultResponse(val token:String,val uid:String)
